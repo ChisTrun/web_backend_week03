49bf790898ec396bdb0183631ca05ecef89f61f8
trungvc <kappa@carem.io>
2024-10-29 20:09:02 +0700

    add git workflow

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,3 @@
-
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
@@ -6,7 +5,20 @@ import { join } from 'path';
 const YAML_CONFIG_FILENAME = 'config.yaml';
 
 export default () => {
-  return yaml.load(
-    readFileSync(join('src','config', YAML_CONFIG_FILENAME), 'utf8'),
+  const config = yaml.load(
+    readFileSync(join('src', 'config', YAML_CONFIG_FILENAME), 'utf8'),
   ) as Record<string, any>;
+
+  const replaceEnvVariables = (obj: any): any => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'string') {
+        obj[key] = obj[key].replace(/\${(.*?)}/g, (_, name) => process.env[name] || '');
+      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+        obj[key] = replaceEnvVariables(obj[key]);
+      }
+    }
+    return obj;
+  };
+
+  return replaceEnvVariables(config);
 };
