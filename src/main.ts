c035dd3bd8932d9157640b04bfaa8b25d13e4a8f
trungvc <kappa@carem.io>
2024-10-30 00:45:12 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    'http://localhost:5173', // Địa chỉ frontend 1
+    '*',
+  ];
   app.enableCors({
-    origin: '*', // Cho phép tất cả các nguồn
+    origin: allowedOrigins, // Cho phép tất cả các nguồn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   })
   await app.listen(process.env.PORT ?? 3000);
