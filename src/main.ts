d87a2479a17ec9d260b885f5902577a0b59ebd54
trungvc <kappa@carem.io>
2024-10-30 01:26:21 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false
+    credentials: true
   })
   await app.listen(process.env.PORT ?? 3000);
 }
